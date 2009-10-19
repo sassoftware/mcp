cb07d741ba76945f463af6ba0e0f4e2361e3319a
Michael Tharp <mtharp@rpath.com>
2009-10-19 12:22:27 -0400

    Re-add jobstatus.py

---
+++ mcp/jobstatus.py
@@ -0,0 +1,15 @@
+#
+# Copyright (c) 2009 rPath, Inc.
+#
+# All rights reserved.
+#
+
+UNKNOWN     =  -1
+WAITING     =   0
+RUNNING     = 100
+BUILT       = 200
+FINISHED    = 300
+FAILED      = 301
+ERROR       = 301
+KILLED      = 302
+NO_JOB      = 401
